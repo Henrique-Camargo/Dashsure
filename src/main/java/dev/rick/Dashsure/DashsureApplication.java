de54f97b33828afd5d7964c76db6bfa7fe74b46e
Henrique-Camargo <henriquecnobrega0711@gmail.com>
2026-01-07 20:17:02 -0300

    first commit

---
+++ src/main/java/dev/rick/Dashsure/DashsureApplication.java
@@ -0,0 +1,13 @@
+package dev.rick.Dashsure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashsureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashsureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rick/Dashsure/DashsureApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rick.Dashsure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashsureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
